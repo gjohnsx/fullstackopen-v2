defc1f00edd3d700c35691c2dacdcf2ff7da6e8c
gjohnsx <gregjohns@fastmail.com>
2022-09-17 15:15:28 -0400

    ex 1.2 complete

---
+++ part1/ex-course-information/src/App.js
@@ -0,0 +1,34 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = {
+    title: 'Half Stack application development',
+    parts: [
+      {part: 'Fundamentals of React', exercises: 10},
+      {part: 'Using props to pass data', exercises: 7},
+      {part: 'State of a component', exercises: 14},
+    ],
+  };
+
+  const totalExercises = course.parts.reduce((acc, cur) => {
+    return acc += cur.exercises;
+  }, 0);
+
+  return (
+    <div>
+      <Header course={course.title} />
+      {course.parts.map(course => {
+        return(
+          <Content key={course.part} part={course.part} exercises={course.exercises} />
+        )
+      })}
+
+      <Total totalExercises={totalExercises} />
+    </div>
+
+  )
+};
+
+export default App;
---
+++ part1/ex-course-information/src/components/Total.js
@@ -0,0 +1,9 @@
+const Total = ({ totalExercises }) => {
+    return(
+      <div>
+        <h2>Number of exercises: {totalExercises}</h2>
+      </div>
+    )
+};
+
+export default Total;
