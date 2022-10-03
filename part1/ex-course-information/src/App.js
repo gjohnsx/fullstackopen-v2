842a91ab6d2ef5f7b08e9da4900c5de0460dcdc3
gjohnsx <gregjohns@fastmail.com>
2022-10-03 12:04:34 -0400

    ex 1.12

--- part1/ex-course-information/src/App.js
+++ part1/ex-course-information/src/App.js
@@ -1,8 +1,45 @@
+import { useState } from 'react';
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Total from "./components/Total";
 
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+};
+
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons.
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+};
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+
+  const handlePlusClick = () => {
+    setCounter(prevCounter => prevCounter + 1);
+    setAllClicks(allClicks.concat('+'));
+  };
+
+  const handleMinusClick = () => {
+    setCounter(prevCounter => prevCounter - 1);
+    setAllClicks(allClicks.concat('-'));
+  };
+
   const course = {
     title: 'Half Stack application development',
     parts: [
@@ -19,6 +56,16 @@ const App = () => {
 
   return (
     <div>
+      <p>
+        {counter}
+      </p>
+      <Button handleClick={handlePlusClick} text='+' />
+      <Button handleClick={() => {
+        setCounter(0);
+        setAllClicks([]);
+      }} text='reset' />
+      <Button handleClick={handleMinusClick} text='-' />
+
       <Header course={course.title} />
       {course.parts.map(course => {
         return(
@@ -27,6 +74,8 @@ const App = () => {
       })}
 
       <Total totalExercises={course.totalEx()} />
+      <History allClicks={allClicks} />
+
     </div>
 
   )
